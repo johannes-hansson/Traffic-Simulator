70de54d3362a38cfe23668ec0a56689055eb704d
johannes-hansson <johanneshansson04@gmail.com>
2026-02-20 11:12:52 +0100

    outline

---
+++ Intersection.java
@@ -0,0 +1,13 @@
+import java.util.Vector;
+
+public class Intersection implements Node {
+    public Road[] getAvailableTurns(Road incoming) {
+        return null;
+    }
+    public Vector<int[]> getLaneMap(Road incoming, Road outgoing) {
+        return null;
+    };
+    public boolean requestTurn(Road incoming, Road outgoing) {
+        return true;
+    };
+}
