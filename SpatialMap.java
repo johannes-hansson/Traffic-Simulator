82a7e509b1630daa36677820fe3a2bd293d0dc3e
johannes-hansson <johanneshansson04@gmail.com>
2026-02-20 11:17:05 +0100

    spatial map

--- SpatialMap.java
+++ SpatialMap.java
@@ -1,4 +1,7 @@
 public class SpatialMap {
+    public boolean isOccupied(RoadPosition position) {
+        return false;
+    }
     public void loadRoads(Road[] roads) {};
     public void addVehicle(Vehicle vehicle, RoadPosition position) {};
     public void moveVehicle(RoadPosition from, RoadPosition to) {};
