5f6474ab9f94dea3b271166b8554794c727f6587
johannes-hansson <johanneshansson04@gmail.com>
2026-02-20 11:10:56 +0100

    outline

--- Road.java
+++ Road.java
@@ -1,3 +1,5 @@
 public class Road {
-    
+    //public Node getEndNode() {};
+    public int getLength() {return 10;};
+    public int getLanes() {return 10;};
 }
---
+++ SpatialMap.java
@@ -0,0 +1,5 @@
+public class SpatialMap {
+    public void loadRoads(Road[] roads) {};
+    public void addVehicle(Vehicle vehicle, RoadPosition position) {};
+    public void moveVehicle(RoadPosition from, RoadPosition to) {};
+}
