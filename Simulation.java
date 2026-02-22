701ec324f8c9ffc0cabfa203af2483ae6802656a
Felix Öhrn <Ohrnen98@gmail.com>
2026-02-22 17:39:36 +0100

    Implement threaded simulation loop

--- Simulation.java
+++ Simulation.java
@@ -3,23 +3,39 @@ import java.util.List;
 
 public class Simulation {
     private Map map;
+    private SpatialMap spatialMap;
     private boolean running = false;
+    private List<Vehicle> vehicles = new ArrayList<>();
     private List<SimulationUpdateListener> updateListeners = new ArrayList<>();
     private int tick = 0;
+    private int tickSpeedMs = 100;
+    private Thread simThread;
 
     public Map getMap() {
         return map;
     }
 
     public void start() {
+        if (running) return; //Security for not starting twice
         running = true;
-        for( int i = 0; i < 20 && running; i++) {
-            step();
-        }
+        simThread = new Thread(() -> { // A new thread that can tick simultaneously parallel
+            while (running) {
+                step();
+                try {
+                    Thread.sleep(tickSpeedMs);
+                } catch (InterruptedException ignored) {
+                    // Interrupting using stop() will get us here
+                }
+            }
+        }, "SimulationThread");
+        simThread.start();
     }
 
     public void stop() {
         running = false;
+        if(simThread != null) {
+            simThread.interrupt(); // Waking it up if it's asleep
+        }
     }
 
     public void addUpdateListener(SimulationUpdateListener listener) {
