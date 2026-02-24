134dc1a109d1e681b80125fd2a79e2a69b23d4e1
DevRanjo <rania.leontiou@outlook.com>
2026-02-24 20:12:16 +0100

    removed boolean

--- VehicleBehaviour.java
+++ VehicleBehaviour.java
@@ -2,20 +2,14 @@ import java.util.ArrayList;
 
 public class VehicleBehaviour {
     
-    
-    private boolean IsBehindVehicle;
-
-    public boolean setIsBehindVehicle(){
-        return IsBehindVehicle = true;
-    }
-
+   
     public ArrayList<LaneSwithDecision> computeLaneSwitches() {
         return null;
     }
 
     public void computeVelocities(ArrayList<Vehicle> vehicles) {
         for (Vehicle vehicle: vehicles) {
-            
+
             VehicleProperties properties = vehicle.getProperties();
             int newVelocity = vehicle.getVelocity() + properties.acceleration(); 
             if(newVelocity < properties.maxVelocity()){
