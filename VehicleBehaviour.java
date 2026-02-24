ea8dea820b50765a47caff63af75e49e7b4ae191
DevRanjo <rania.leontiou@outlook.com>
2026-02-24 16:46:00 +0100

    Removed commented code

--- VehicleBehaviour.java
+++ VehicleBehaviour.java
@@ -5,7 +5,6 @@ public class VehicleBehaviour {
     
     private boolean IsBehindVehicle;
 
-
     public boolean setIsBehindVehicle(){
         return IsBehindVehicle = true;
     }
@@ -22,37 +21,13 @@ public class VehicleBehaviour {
         }
     }
 
-    /*
-    public void computeLaneSwitches(Vehicle[] vehicles, LocationalMap locationalMap, 
-                LaneSwitchDecision[] laneSwitchDecisions){
-                    
-                    int n_vehicles = getVehicleAmount();
-                    
-                    //all vehicles with vehicles in front -> store in vehiclesWithObstacles
-
-            for(int i=0; i < n_vehicles; i++){
-                
-                double velocityThisVehicle = vehicles[i].velocity;
-
-                        RoadPosition positionVehicle = vehicles[i].getVehiclePositionRoad();
-                        Road road = positionVehicle.getRoad();
-                        int roadLength = road.getLength();
-                        
-                        //check if vehicle is in front
-                        
-                        
-                        if (){
-                            boolean behindVehicle = setIsBehindVehicle();
-                        }
-                    }
-                }
-        */
+    
 
     public void computeLaneSwitches(Vehicle[] vehicles, LocationalMap locationalMap, 
                 LaneSwitchDecision[] laneSwitchDecisions){
                     
                 
                 }
-        */
+    
 
 }
