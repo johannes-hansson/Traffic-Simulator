a2b1abae9fcb2eb90fb1bcdb6a52f1c734c9ec8a
Johannes Hansson <johanneshansson04@gmail.com>
2026-02-26 20:57:41 +0100

    Implemented a method to move a vehicle while updating the roads

--- VehicleMovement.java
+++ VehicleMovement.java
@@ -2,13 +2,62 @@ import java.util.List;
 
 public class VehicleMovement {
 
-<<<<<<< HEAD
-    public ArrayList<Vehicle> move(ArrayList<Vehicle> vehicles, LocationalMap locationMap, Simulation sim) {
+    private void moveVehicle(Vehicle vehicle, RoadPosition position) {
+        RoadPosition currentPosition = vehicle.getPosition();
+
+        Road currentRoad = currentPosition.road();
+        Road newRoad = position.road();
+
+        // Edge case for when the vehicle was not on a previous road
+        // and did not enter a new road
+        // This edge case should never occur
+        if (currentRoad == null && newRoad == null) {
+            vehicle.setPosition(position);
+            return;
+        }
+
+        boolean changedRoads = currentRoad != newRoad;
+
+        // If the current road is not null, and the current road is different
+        // from the new road, then the vehicle must have left the current road
+        boolean exitedCurrentRoad = (currentRoad != null) && (changedRoads == false);
+
+        // If the new road is not null, and the new road is different
+        // from the current road, then the vehicle must have entered the new road
+        boolean enteredNewRoad = (newRoad != null) && (changedRoads == false);
+
+        if (exitedCurrentRoad) {
+            currentRoad.removeVehicleAt(currentPosition.lane(), currentPosition.cell());
+        }
+
+        if (enteredNewRoad) {
+            newRoad.enterVehicle(vehicle, position.lane(), position.cell());
+        }
+
+        // By this point, it has been confirmed that the current road and the new road
+        // are not both null
+        // If they are the same, then neither of them can be null and the vehicle must have moved
+        // within the same road 
+        if (changedRoads == false) {
+            newRoad.moveVehicle(
+                currentPosition.lane(), 
+                currentPosition.cell(),
+                position.lane(),
+                position.cell()
+            );
+        }
+
+        vehicle.setPosition(position);
+        
+    }
+
+    public void process(List<Vehicle> vehicles) {
+
+    }
+
+    public void move(List<Vehicle> vehicles, LocationalMap locationMap, Simulation sim) {
         VehicleBehaviour vehicleBehaviour = new VehicleBehaviour();
 
-=======
-    public void move(List<Vehicle> vehicles, LocationalMap locationMap) {
->>>>>>> main
         for (Vehicle vehicle : vehicles) {
         ///for loop for all vehicles here
             RoadPosition position = vehicle.getPosition();
@@ -34,7 +83,6 @@ public class VehicleMovement {
         locationMap.moveVehicle(position, newPosition);
         vehicle.setPosition(newPosition);
         }
-        return vehicles;
     }    
 
      /* Nagel-schreckenberg-model
