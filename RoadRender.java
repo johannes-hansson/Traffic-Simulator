80cb9732dc2bad605682a37ae1f0ae7b426de3ac
Malin Gelin <malgeh241@studentmail.oru.se>
2026-02-28 13:31:56 +0100

    Added draw method to RoadRender

--- RoadRender.java
+++ RoadRender.java
@@ -1,5 +1,10 @@
 import java.util.ArrayList;
 
+import javafx.scene.layout.Pane;
+import javafx.scene.shape.Line;
+import javafx.scene.paint.Color;
+
+
 // Visuell representation av en väg
 public class RoadRender {
     ArrayList<BreakPoint> breakPoints; // Punkter som vägen ritas mellan
@@ -30,4 +35,30 @@ public class RoadRender {
     public ArrayList<BreakPoint> getBreakPoints() {
         return this.breakPoints;
     }
+
+    public void draw(Pane root) {
+
+        // Draw between each pair of points
+        for (int i = 0; i < breakPoints.size() - 1; i++){
+            BreakPoint p1 = breakPoints.get(i); // get first point (start)
+            BreakPoint p2 = breakPoints.get(i + 1); // get second point (end)
+
+            Line roadSegment = new Line( // here we create the line
+                    p1.x(), p1.y(),
+                    p2.x(), p2.y()
+            );
+
+            roadSegment.setStrokeWidth(width); // width of road
+            roadSegment.setStroke(Color.DARKGRAY); // color of road
+
+            root.getChildren().add(roadSegment);
+            /*
+            root = Pane = drawing surface / empty box
+            getChildren = all that is within the box
+            add = adds our lines
+            */
+        }
+
+    }
+
 }
