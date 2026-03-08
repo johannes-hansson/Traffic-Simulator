7511140d1551450fb981ec671ec84da5b574c500
Johannes Hansson <46356276+johannes-hansson@users.noreply.github.com>
2026-03-08 18:06:38 +0100

    Added a method for turning

--- VehicleBehaviour.java
+++ VehicleBehaviour.java
@@ -8,6 +8,56 @@ public class VehicleBehaviour {
    //     return null;
    // }
 
+    // Takes a node that the vehicle has reached and computes 
+    // amount of cells the vehicle can travel after the node
+    // Returns 0 if the vehicle can not enter the node
+    public int getFreeDistanceAfterTurn(
+        Road road,
+        int lane,
+        Vehicle vehicle,
+        int maxDistance
+    ) {
+
+        Node node = road.getEndNode();
+        Road roadToEnter = vehicle.chooseRoad(node.getAvailableTurns(road));
+
+        boolean canTurn = node.requestTurn(road, roadToEnter);
+        if (canTurn == false) {
+            return 0;
+        }
+
+        // If the lane to enter is not reassigned from -1, 
+        // the turn cannot be made from the current lane
+        int laneToEnter = -1; 
+
+        int[][] laneMap = node.getLaneMap(road, roadToEnter);
+        for (int i = 0; i < laneMap.length; i++) {
+            if (laneMap[i][0] == lane) {
+                laneToEnter = laneMap[i][1];
+                break;
+            }
+        }
+        if (laneToEnter == -1) {
+            return 0;
+        }
+
+        // If the vehicle is able to make the turn, scan the following road
+        Road.ScanResult scanResult = roadToEnter.scanCells(laneToEnter, 0, maxDistance, true);
+        
+        // If the scan reached the end of the road, call the method recursively
+        // to get the free distance on the next road
+        if (scanResult.endOfRoadReached()) {
+            return scanResult.distance() + this.getFreeDistanceAfterTurn(
+                roadToEnter, 
+                laneToEnter, 
+                vehicle, 
+                maxDistance - scanResult.distance()
+            );
+        }
+
+        return scanResult.distance();
+    }
+
     public int accelerate(Vehicle vehicle) { //Acceleration: vi <- min (vi+1,vmax)
         VehicleProperties properties = vehicle.getProperties();
         int prevVelocity = vehicle.getVelocity();
@@ -24,13 +74,24 @@ public class VehicleBehaviour {
         int velocity = vehicle.getVelocity();
         RoadPosition position = vehicle.getPosition();
         Road road = position.road();
-        int lane = road.getLanes();
+        int lane = position.lane();
 
         //scan the road (get the free gap between in front of vehicle/intersection)
         Road.ScanResult scanResult = road.scanCells(lane, position.cell(), velocity, false);
-        velocity = scanResult.distance();
+        gap = scanResult.distance();
+
+        if (scanResult.endOfRoadReached()) {
+            int remainingDistance = velocity - gap;
+            gap += this.getFreeDistanceAfterTurn(
+                road, 
+                lane, 
+                vehicle, 
+                remainingDistance
+            );
+        }
        
         //the new velocity is v = min(v, gap) where gap is the free cells until next vehicle__
+        velocity = gap;
         return velocity;
     }
 
