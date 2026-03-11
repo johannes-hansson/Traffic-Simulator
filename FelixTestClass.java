0ee9d5cd9148ce3befecee50ed2cd26869368feb
Felix Öhrn <Ohrnen98@gmail.com>
2026-03-11 20:04:01 +0100

    fix traffic light lane scanning and add MockNode integration test

--- FelixTestClass.java
+++ FelixTestClass.java
@@ -2,6 +2,46 @@ public class FelixTestClass {
 
     public static void main(String[] args) throws InterruptedException {
 
+        MockNode node = new MockNode(new double[]{0, 0}, 50);
+
+        Road inNorth = new Road(node, 100, 1, null, "inNorth");
+        Road inEast  = new Road(node, 100, 1, null, "inEast");
+        Road outSouth = new Road(node, 100, 1, null, "outSouth");
+        Road outWest  = new Road(node, 100, 1, null, "outWest");
+
+        node.addIncomingRoad(inNorth, CardinalDirection.NORTH);
+        node.addIncomingRoad(inEast, CardinalDirection.EAST);
+        node.addOutgoingRoad(outSouth, CardinalDirection.SOUTH);
+        node.addOutgoingRoad(outWest, CardinalDirection.WEST);
+
+        // Lägg en bil nära slutet av inEast så att det finns incoming traffic där
+        RoadPosition pos = new RoadPosition(inEast, 0, 95);
+        VehicleProperties props = new VehicleProperties(5, 1, 1);
+        Vehicle vehicle = new Vehicle(props, pos, 0);
+        inEast.enterVehicle(vehicle, 0, 95);
+
+        System.out.println("Initial open roads: " + node.getOpenRoads().size());
+        System.out.println("Initial closed roads: " + node.getClosedRoads().size());
+
+        for (int i = 1; i <= 10; i++) {
+            node.getTrafficLight().update();
+
+            System.out.println("Tick " + i);
+            System.out.println("Open roads:");
+            for (Road road : node.getOpenRoads()) {
+                System.out.println("  " + road.name);
+            }
+
+            System.out.println("Closed roads:");
+            for (Road road : node.getClosedRoads()) {
+                System.out.println("  " + road.name);
+            }
+
+            System.out.println("Can inNorth turn? " + node.requestTurn(inNorth, outSouth));
+            System.out.println("Can inEast turn? " + node.requestTurn(inEast, outSouth));
+            System.out.println();
+        }
+
        /* // Build a ready-to-run demo simulation (map + behaviour + movement + 2 cars + stats + debug listeners)
         Simulation sim = Simulation.createDemoWithStats();
 
@@ -32,7 +72,7 @@ public class FelixTestClass {
         Thread.sleep(50);
         */
 
-        MockNode node = new MockNode(new double[]{0, 0}, 50);
+        /* MockNode node = new MockNode(new double[]{0, 0}, 50);
 
         Road road1 = new Road(node, 100, 1, null, "road1");
         Road road2 = new Road(node, 100, 1, null, "road2");
@@ -61,5 +101,6 @@ public class FelixTestClass {
             System.out.println("greenTime = " + light.getCurrentGreenTime());
             System.out.println();
         }
+         */
     }
 }
